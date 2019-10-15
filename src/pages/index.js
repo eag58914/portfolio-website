e8c545b2a059544a6eb7dd3181972c9b9b8b8c30
Elco Garcia <garciaelco18@gmail.com>
2019-10-15 14:22:02 -0500

    testing snap scrolling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import Header from '../components/Header/header';
+import(`../styles/main.scss`);
 
 const IndexPage = () => (
 	<div>
-		<Header />
+		<section>
+			<h3>A subtitle lives here </h3>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta doloremque mollitia ad commodi, eaque
+				distinctio possimus veniam aliquam fugit? Fugiat fuga numquam sequi eligendi quae! Reiciendis dolor
+				molestiae at ipsum?
+			</p>
+		</section>
+		<section>
+			<h3>A subtitle lives here </h3>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta doloremque mollitia ad commodi, eaque
+				distinctio possimus veniam aliquam fugit? Fugiat fuga numquam sequi eligendi quae! Reiciendis dolor
+				molestiae at ipsum?
+			</p>
+		</section>
+		<section>
+			<h3>A subtitle lives here </h3>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta doloremque mollitia ad commodi, eaque
+				distinctio possimus veniam aliquam fugit? Fugiat fuga numquam sequi eligendi quae! Reiciendis dolor
+				molestiae at ipsum?
+			</p>
+		</section>
 	</div>
 );
 
