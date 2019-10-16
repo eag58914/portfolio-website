99b07d18c95d602ea95aa4159409305ab34855b4
Elco Garcia <garciaelco18@gmail.com>
2019-10-16 15:44:01 -0500

    made function into a component class

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TitlePage from '../components/TitlePage/TitlePage';
+import PortfolioPage from '../components/Potfolio/PortfolioPage';
 import(`../styles/main.scss`);
 
-const IndexPage = () => (
-	<div>
-		<section>
-			<h3>A subtitle lives here </h3>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta doloremque mollitia ad commodi, eaque
-				distinctio possimus veniam aliquam fugit? Fugiat fuga numquam sequi eligendi quae! Reiciendis dolor
-				molestiae at ipsum?
-			</p>
-		</section>
-		<section>
-			<h3>A subtitle lives here </h3>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta doloremque mollitia ad commodi, eaque
-				distinctio possimus veniam aliquam fugit? Fugiat fuga numquam sequi eligendi quae! Reiciendis dolor
-				molestiae at ipsum?
-			</p>
-		</section>
-		<section>
-			<h3>A subtitle lives here </h3>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta doloremque mollitia ad commodi, eaque
-				distinctio possimus veniam aliquam fugit? Fugiat fuga numquam sequi eligendi quae! Reiciendis dolor
-				molestiae at ipsum?
-			</p>
-		</section>
-	</div>
-);
+class IndexPage extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<TitlePage />
+				<PortfolioPage />
+			</div>
+		);
+	}
+}
 
 export default IndexPage;
