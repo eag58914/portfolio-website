76c6e90ce715091c3f652c0e7d5a0a80f3bb9082
Elco Garcia <garciaelco18@gmail.com>
2019-10-16 13:58:23 -0500

    created titlepage component

---
+++ src/components/TitlePage/TitlePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import('../TitlePage/TitlePage.scss');
+
+const TitlePage = () => {
+	return (
+		<div className="title">
+			<h1>Elco Garcia</h1>
+			<h1>Thinker</h1>
+			<h1>Developer</h1>
+		</div>
+	);
+};
+
+export default TitlePage;
