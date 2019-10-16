42582e54e61ef74d05b558963c7ac8c2197f5554
Elco Garcia <garciaelco18@gmail.com>
2019-10-16 16:16:08 -0500

    created different sections for portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import TitlePage from '../components/TitlePage/TitlePage';
-import PortfolioPage from '../components/Potfolio/PortfolioPage';
+import PortfolioPage from '../components/Portfolio/PortfolioPage';
 import(`../styles/main.scss`);
 
 class IndexPage extends Component {
-	constructor(props) {
-		super(props);
-	}
 	render() {
 		return (
-			<div>
-				<TitlePage />
-				<PortfolioPage />
-			</div>
+			<body>
+				<section>
+					<TitlePage />
+				</section>
+				<section>
+					<PortfolioPage />
+				</section>
+			</body>
 		);
 	}
 }
