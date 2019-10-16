77336baedd9583897fceb2d240f97ab74e34a485
Elco Garcia <garciaelco18@gmail.com>
2019-10-16 16:15:28 -0500

    moved pages

--- src/components/Potfolio/PortfolioPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import PortfolioCSS from '../Potfolio/Portfolio.scss';
-const Portfolio = () => {
-	return <h1 className="p-title">Portfolio</h1>;
-};
-
-export default Portfolio;
