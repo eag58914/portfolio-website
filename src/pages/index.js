db49e8ce4ff300c6926553e5abdb0095f60da87d
Elco Garcia <garciaelco18@gmail.com>
2019-12-31 17:24:35 -0600

    imported Nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TitlePage from '../components/TitlePage/TitlePage';
 import PortfolioPage from '../components/Portfolio/PortfolioPage';
 import About from '../components/About/AboutPage';
+import Nav from '../components/Nav/Nav';
 import(`../styles/main.scss`);
 
 class IndexPage extends Component {
@@ -11,6 +12,7 @@ class IndexPage extends Component {
 				<section>
 					<TitlePage />
 				</section>
+				<Nav />
 				<section>
 					<About />
 				</section>
