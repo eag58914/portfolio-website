549209fd3980c68496c6b893b96c0f89771dbc2f
Elco Garcia <garciaelco18@gmail.com>
2020-01-12 13:57:46 -0600

    title for main page

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Title.module.css';
+
+const Title = () => {
+	return (
+		<div className="flex-container">
+			<h1 className={style.titleText}>EGA</h1>
+		</div>
+	);
+};
+
+export default Title;
