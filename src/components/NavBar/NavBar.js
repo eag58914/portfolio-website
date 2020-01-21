d32646ed4d00ad45e42f080b84e9bcc6452f5fa6
Elco Garcia <garciaelco18@gmail.com>
2020-01-21 12:24:02 -0600

    creating links  to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from './NavBar.module.css';
+import Link from 'gatsby-link';
 
 const NavBar = () => {
 	return (
 		<div className={style.navBar}>
-			<h2 className={style.navItem}>About</h2>
+			<Link to="/about" className={style.navItem}>
+				About
+			</Link>
 			<h2 className={style.navItem}>Resume</h2>
 			<h2 className={style.navItem}>Contact</h2>
 		</div>
