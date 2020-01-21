3d14c5395e38e440d3edba1d71156a45f68ce612
Elco Garcia <garciaelco18@gmail.com>
2020-01-21 13:04:01 -0600

    google fonts package

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,9 +27,14 @@ module.exports = {
 				icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
 			}
 		},
-		`gatsby-plugin-sass`
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.dev/offline
-		// `gatsby-plugin-offline`,
+		`gatsby-plugin-sass`,
+		{
+			resolve: `gatsby-plugin-google-fonts`,
+			options: {
+				fonts: `Roboto Slab`,
+				serif
+			},
+			display: 'swap'
+		}
 	]
 };
