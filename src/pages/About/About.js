444578abb358fee2d948d507db237c1860d6c0dd
Elco Garcia <garciaelco18@gmail.com>
2020-01-21 12:25:03 -0600

    created new pages to be made

--- src/pages/About/About.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const About = () => {
-	return <div />;
-};
-
-export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Title from '../components/Title/Title';
 import NavBar from '../components/NavBar/NavBar';
-import Portfolio from '../components/Porfolio/Portfolio';
+import Portfolio from '../components/Portfolio/Portfolio';
 import(`../styles/main.scss`);
 
 class IndexPage extends Component {
